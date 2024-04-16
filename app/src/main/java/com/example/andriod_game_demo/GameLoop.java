7541970ac9fd86baabc4f0687d8a7813e1dbc0ca
Sithum Senanayake <127540055+IT22305282@users.noreply.github.com>
2024-04-17 00:42:01 +0530

    Added FPS checker

--- app/src/main/java/com/example/andriod_game_demo/GameLoop.java
+++ app/src/main/java/com/example/andriod_game_demo/GameLoop.java
@@ -12,9 +12,19 @@ public class GameLoop implements Runnable{
     @Override
     public void run() {
         long lastFPScheck = System.currentTimeMillis();
+        int fps = 0;
+
         while(true){
             gamePanel.update();
             gamePanel.render();
+            fps++;
+
+            long now = System.currentTimeMillis();
+            if(now - lastFPScheck >= 1000){
+                System.out.println("FPS: " + fps + " " + System.currentTimeMillis());
+                fps = 0;
+                lastFPScheck += 1000;
+            }
         }
     }
 
